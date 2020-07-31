cf9ded286baf78bd7175997ece471d1f5962553f
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-07-31 12:40:11 +0200

    optimize code for monthSelector.js and divide it

--- shift-viewer-v.1.0/src/components/DateSelector/components/MonthSelector.js
+++ shift-viewer-v.1.0/src/components/DateSelector/components/MonthSelector.js
@@ -1,4 +1,5 @@
 import React from "react";
+import monthNames from "../../../services/monthNames";
 
 const MonthSelector = (props) => {
   return (
@@ -10,18 +11,11 @@ const MonthSelector = (props) => {
         id="months"
         onChange={(e) => props.handler(e.target.value)}
       >
-        <option value="0">Styczeń</option>
-        <option value="1">Luty</option>
-        <option value="2">Marzec</option>
-        <option value="3">Kwiecień</option>
-        <option value="4">Maj</option>
-        <option value="5">Czerwiec</option>
-        <option value="6">Lipiec</option>
-        <option value="7">Sierpień</option>
-        <option value="8">Wrzesień</option>
-        <option value="9">Październik</option>
-        <option value="10">Listopad</option>
-        <option value="11">Grudzień</option>
+        {monthNames.map((month, i) => (
+          <option value={i} key={i}>
+            {month}
+          </option>
+        ))}
       </select>
     </div>
   );
--- shift-viewer-v.1.0/src/components/DateSelector/components/YearSelector.js
+++ shift-viewer-v.1.0/src/components/DateSelector/components/YearSelector.js
@@ -3,12 +3,6 @@ import React from "react";
 const YearSelector = (props) => {
   return (
     <div>
-      {/* <span>Rok: </span>
-      <input
-        placeholder="wpisz rok"
-        value={props.year}
-        onChange={(e) => props.handler(e.target.value)}
-      ></input> */}
       <span onClick={() => props.handler("-")}>◄</span>
       <span>{props.year}</span>
       <span onClick={() => props.handler("+")}>►</span>
