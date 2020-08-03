ace32edd7344d99a022503b389d3d59bf1b3eb53
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-08-03 21:39:28 +0200

    Updates

---
+++ precache-manifest.5fbedc4ee7f612176b0ac16ebd5ea588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a9a07208f03af1d1c427894c1445f2",
+    "url": "/shift-viewer/index.html"
+  },
+  {
+    "revision": "6cbb17940066ba8164d1",
+    "url": "/shift-viewer/static/css/main.1ad60077.chunk.css"
+  },
+  {
+    "revision": "1dc7f1088c2ff2f8775e",
+    "url": "/shift-viewer/static/js/2.ce20cb49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shift-viewer/static/js/2.ce20cb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cbb17940066ba8164d1",
+    "url": "/shift-viewer/static/js/main.ffe48302.chunk.js"
+  },
+  {
+    "revision": "65770e5fafc25efac525",
+    "url": "/shift-viewer/static/js/runtime-main.c548c96b.js"
+  },
+  {
+    "revision": "e1eb63edee5cc93db7a03943b799d8fd",
+    "url": "/shift-viewer/static/media/TREBUCBD.e1eb63ed.ttf"
+  }
+]);
