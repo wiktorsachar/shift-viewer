fc995b546cc380513d4ecfec94e8dfe0415a8305
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-09-20 19:32:41 +0200

    layout refactored, added help sign

--- shift-viewer-v.2.0 - official/src/components/DateSelector/components/ShiftSelector.js
+++ shift-viewer-v.2.0 - official/src/components/DateSelector/components/ShiftSelector.js
@@ -1,31 +1,37 @@
 import React from "react";
+import "./ShiftSelector.css";
 
 const ShiftSelector = (props) => {
   return (
-    <div>
-      <span>System: </span>
-      <select
-        name="system"
-        value={props.system}
-        id="systems"
-        onChange={(e) => {
-          props.handleSystemChange(e.target.value);
-        }}
-      >
-        <option value="blank">wybierz system</option>
-        <option value="2shift">dwuzmianowy</option>
-        <option value="4shift">czterozmianowy</option>
-      </select>
-      <br />
-      <span>Zmiana: </span>
-      <input
-        type="date"
-        value={props.date}
-        onChange={(e) => {
-          props.handleDateChange(e.target.value);
-        }}
-      ></input>
-    </div>
+    <React.Fragment>
+      <div>
+        <span>System: </span>
+        <select
+          name="system"
+          value={props.system}
+          id="systems"
+          onChange={(e) => {
+            props.handleSystemChange(e.target.value);
+          }}
+        >
+          <option value="blank">wybierz system</option>
+          <option value="2shift">dwuzmianowy</option>
+          <option value="4shift">czterozmianowy</option>
+        </select>
+      </div>
+      <div className="shift-creator">
+        <p className="shift-title">Zmiana dzienna: </p>
+        <input
+          type="date"
+          value={props.date}
+          onChange={(e) => {
+            props.handleDateChange(e.target.value);
+          }}
+          id="shift-date"
+        ></input>
+        <div id="help">�</div>
+      </div>
+    </React.Fragment>
   );
 };
 
