429f22134c4940e03066c5792aec8b392479ecf5
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-09-20 22:15:41 +0200

    add help button and popup

--- shift-viewer-v.2.0 - official/src/components/DateSelector/components/ShiftSelector.js
+++ shift-viewer-v.2.0 - official/src/components/DateSelector/components/ShiftSelector.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./ShiftSelector.css";
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
 
 const ShiftSelector = (props) => {
+//  const toggleClass = (event) => {
+//     event.target.classList.toggle('show')
+//  }
   return (
     <React.Fragment>
       <div>
@@ -29,10 +36,19 @@ const ShiftSelector = (props) => {
           }}
           id="shift-date"
         ></input>
-        <div id="help">�</div>
+        {/* <div id="help">�</div> */}
+        <div className="popup" onClick={myFunction/*e=>toggleClass(e)*/}>
+          �
+          <span className="popuptext" id="myPopup">
+            Wybierz dowolny dzień, w którym Twoja zmiana miała służbę dzienną. 
+          </span>
+        </div>
       </div>
     </React.Fragment>
   );
 };
 
 export default ShiftSelector;
+
+//kolor napisów zmienić na niebieski-sw
+//dodać popup do znaku zapytania
