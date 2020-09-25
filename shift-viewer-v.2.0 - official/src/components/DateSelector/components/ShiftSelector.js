c0452e52d7d30339dff9baf38b22ccbef5904b0b
DESKTOP-TJ00H46\Herbatnik <viksatcher@gmail.com>
2020-09-25 20:26:00 +0200

    optimalize popup class toggle

--- shift-viewer-v.2.0 - official/src/components/DateSelector/components/ShiftSelector.js
+++ shift-viewer-v.2.0 - official/src/components/DateSelector/components/ShiftSelector.js
@@ -1,54 +1,56 @@
 import React from "react";
 import "./ShiftSelector.css";
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
 
-const ShiftSelector = (props) => {
-//  const toggleClass = (event) => {
-//     event.target.classList.toggle('show')
-//  }
-  return (
-    <React.Fragment>
-      <div>
-        <span>System: </span>
-        <select
-          name="system"
-          value={props.system}
-          id="systems"
-          onChange={(e) => {
-            props.handleSystemChange(e.target.value);
-          }}
-        >
-          <option value="blank">wybierz system</option>
-          <option value="2shift">dwuzmianowy</option>
-          <option value="4shift">czterozmianowy</option>
-        </select>
-      </div>
-      <div className="shift-creator">
-        <p className="shift-title">Zmiana dzienna: </p>
-        <input
-          type="date"
-          value={props.date}
-          onChange={(e) => {
-            props.handleDateChange(e.target.value);
-          }}
-          id="shift-date"
-        ></input>
-        {/* <div id="help">�</div> */}
-        <div className="popup" onClick={myFunction/*e=>toggleClass(e)*/}>
-          �
-          <span className="popuptext" id="myPopup">
-            Wybierz dowolny dzień, w którym Twoja zmiana miała służbę dzienną. 
-          </span>
+class ShiftSelector extends React.Component {
+  state = {
+    popupStatus: "popuptext",
+  };
+  changePopupStatus = () => {
+    this.state.popupStatus === "popuptext"
+      ? this.setState({ popupStatus: "popuptext show" })
+      : this.setState({ popupStatus: "popuptext" });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <span>System: </span>
+          <select
+            name="system"
+            value={this.props.system}
+            id="systems"
+            onChange={(e) => {
+              this.props.handleSystemChange(e.target.value);
+            }}
+          >
+            <option value="blank">wybierz system</option>
+            <option value="2shift">dwuzmianowy</option>
+            <option value="4shift">czterozmianowy</option>
+          </select>
         </div>
-      </div>
-    </React.Fragment>
-  );
-};
+        <div className="shift-creator">
+          <p className="shift-title">Zmiana dzienna: </p>
+          <input
+            type="date"
+            value={this.props.date}
+            onChange={(e) => {
+              this.props.handleDateChange(e.target.value);
+            }}
+            id="shift-date"
+          ></input>
+          <div className="popup" onClick={this.changePopupStatus}>
+            �
+            <span className={this.state.popupStatus} id="myPopup">
+              Wybierz dowolny dzień, w którym Twoja zmiana miała służbę dzienną.
+            </span>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default ShiftSelector;
 
 //kolor napisów zmienić na niebieski-sw
-//dodać popup do znaku zapytania
+//zrobić ukrywanie popupu po kliknięciu gdziekolwiek
